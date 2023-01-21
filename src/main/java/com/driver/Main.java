86f5447cdb59614b4b76837e70647e609f22a19a
SakT1122 <sak.978tirbhane@gmail.com>
2023-01-21 13:13:00 +0530

    new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     RWOnly obj = new RWOnly();
-        obj.setName("sakshi");
-        obj.getName();
+    obj.setName("sakshi");
+    obj.getName();
   
 }
